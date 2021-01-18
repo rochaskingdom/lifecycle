0097878184c1657894ff1744028611179aac6081
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-01-17 21:15:06 -0300

    Eventos visuais

--- src/app/main-lifecycle/life-cycle-child/life-cycle-child.component.ts
+++ src/app/main-lifecycle/life-cycle-child/life-cycle-child.component.ts
@@ -9,6 +9,12 @@ import {
   SimpleChanges
 } from '@angular/core';
 
+export interface LifeCycleEvent {
+  id: number;
+  name: string;
+  color: string;
+}
+
 @Component({
   selector: 'app-life-cycle-child',
   templateUrl: './life-cycle-child.component.html',
@@ -20,28 +26,53 @@ export class LifeCycleChildComponent implements OnInit, OnDestroy, OnChanges, Af
   @Input() age: number;
   @Input() food: string;
 
+  events: LifeCycleEvent[] = [];
+  nextEventId = 0;
+  colors = ['accent', 'warn', 'primary'];
+
   constructor() {
     console.log(this.name, '- constructor');
+    this.newEvent('constructor');
   }
 
   ngOnInit(): void {
     console.log(this.name, '- ngOnInit');
+    this.newEvent('ngOnInit');
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(this.name, '- ngOnChanges');
+    this.newEvent('ngOnChanges');
   }
 
   ngAfterContentInit(): void {
     console.log(this.name, '- ngAfterContentInit');
+    this.newEvent('ngAfterContentInit');
   }
 
   ngAfterViewInit(): void {
     console.log(this.name, '- ngAfterViewInit');
+    this.newEvent('ngAfterViewInit');
   }
 
   ngOnDestroy(): void {
     console.log(this.name, '- ngOnDestroy');
+    this.newEvent('ngOnDestroy');
+  }
+
+  newEvent(name: string): void {
+    const id = this.nextEventId++;
+    this.events.push({
+      id,
+      color: this.colors[id % this.colors.length],
+      name
+    });
+    setTimeout(() => {
+      const idx = this.events.findIndex((e) => e.id === id);
+      if (idx >= 0) {
+        this.events.splice(idx, 1);
+      }
+    }, 3000 + this.events.length * 2000);
   }
 
 }
